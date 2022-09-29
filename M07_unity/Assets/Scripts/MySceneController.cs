aa0c5a432130189e26369698d3d73a63ec587a1f
Sam <marcin.kasica@gmail.com>
2022-09-29 10:17:24 -0500

    Added Script component

---
+++ M07_unity/Assets/Scripts/MySceneController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; // this is an element allowing to use elements of Unity
+public class MySceneController : MonoBehaviour
+{
+    int healthValue; // this will store the health value between 0-100 of a player
+
+    
+
+
+    private void Start()
+    {
+        Debug.Log("Marcin is the best teacher");
+    }
+}
