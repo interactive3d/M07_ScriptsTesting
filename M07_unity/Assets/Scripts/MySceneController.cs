944ab560a01540d6437eff12844a47db3eb437b2
Sam <marcin.kasica@gmail.com>
2022-09-29 12:08:19 -0500

    Update MySceneController.cs

--- M07_unity/Assets/Scripts/MySceneController.cs
+++ M07_unity/Assets/Scripts/MySceneController.cs
@@ -7,6 +7,10 @@ public class MySceneController : MonoBehaviour
     public string userName = "Marcin";
     public float yourMoney; // this store the float value for amount of collected gold in the application
 
+    public string[] artifacts = {"Cup", "Gold Coin", "Treasure Chest"}; // this will store elements that user collects in the game
+
+    public string[] otherUsers;
+
     private void Start()
     {
         // Debug.Log("Marcin is the best teacher");
@@ -17,5 +21,20 @@ public class MySceneController : MonoBehaviour
         int numberOfClicks = 0;
         numberOfClicks = 10;
         Debug.Log("You have clicked " + numberOfClicks + " times");
+
+        string myArtifactsToCollect = ""; // this is currently an empty string type data
+        
+        // this loop will go over each element of the array called artifacts
+        for (int i = 0; i< artifacts.Length; i++)
+        {
+            myArtifactsToCollect = myArtifactsToCollect + artifacts[i] + ", ";
+        }
+
+        Debug.Log("You task in the game is to collect " + myArtifactsToCollect);
+
+        /*
+         * Create an array that consist "Cup", "Gold Coin", "Treasure Chest"
+         * At the start of the game. Display a message "You task in the game is to find: "  + elements of the array
+         */
     }
 }
