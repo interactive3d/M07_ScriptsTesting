1c0995e87c3716c7ed691d047b2c0f605dc255bb
Sam <marcin.kasica@gmail.com>
2022-09-29 11:05:49 -0500

    Update MySceneController.cs

--- M07_unity/Assets/Scripts/MySceneController.cs
+++ M07_unity/Assets/Scripts/MySceneController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine; // this is an element allowing to use elements of Unity
 public class MySceneController : MonoBehaviour
 {
-    int healthValue; // this will store the health value between 0-100 of a player
-
-    
-
+    public int healthValue; // this will store the health value between 0-100 of a player
+    public string userName = "Marcin";
+    public float yourMoney; // this store the float value for amount of collected gold in the application
 
     private void Start()
     {
-        Debug.Log("Marcin is the best teacher");
+        // Debug.Log("Marcin is the best teacher");
+        healthValue = 100;
+        yourMoney = 0.0f; 
+        Debug.Log(userName + " - your health value is " + healthValue + "%");
+
+        int numberOfClicks = 0;
+        numberOfClicks = 10;
+        Debug.Log("You have clicked " + numberOfClicks + " times");
     }
 }
